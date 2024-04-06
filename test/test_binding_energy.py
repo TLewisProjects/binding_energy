6ba354d232cc7d04f6e4c6d062e3592c3595ba5d
Tom Lewis <tomlewis9766@gmail.com>
2024-04-06 20:33:56 +0100

    Adding further test cases that utilise particle framework.

--- test/test_binding_energy.py
+++ test/test_binding_energy.py
@@ -1,12 +1,28 @@
 import unittest
-import math
+import os
 
 from numpy.testing import assert_allclose
 from binding_energy import binding_energy
 
+from binding_energy.particles import Particles
+
+script_dir = os.path.dirname(__file__)
+
 class TestBindingEnergy(unittest.TestCase):
     def test_binding_energy(self):
         assert_allclose(binding_energy.binding_energy(6.82e-10), -1.0e-22, rtol=1e-1)
+
+    def test_initial_test(self):
+        assert_allclose(binding_energy.initial_test(), 3.819639045e-18)
+
+    def test_zero_system_2(self):
+        test_system = Particles(os.path.join(script_dir, "zero_system_2.txt"))
+        assert_allclose(test_system.binding_energy(ev=True), 0.0)
+
+    def test_zero_system_3(self):
+        test_system = Particles(os.path.join(script_dir, "zero_system_3.txt"))
+        assert_allclose(test_system.binding_energy(ev=True), 0.0, atol=1e-16)
+
 
 if __name__ == "__main__":
     unittest.main()
